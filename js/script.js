6f5084838a98ef10dde31265fcbfeddefce5565e
MarcinDraus <marcin.drauss@gmail.com>
2022-12-30 21:35:26 +0100

    playerMove

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-
+let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -15,19 +15,17 @@ else if(randomNumber == 3) {
 }
 printMessage('Mój ruch to: ' + computerMove);
 
-
-
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
-  computerMove = 'kamień';
-} else if(playerInput == '2')
-    {computerMove = 'papier'
-}else if(playerInput == '3'){
-    computerMove = 'nozyce'
-}
-
-printMessage('Twój ruch to: ' + playerInput);
+    playerMove = 'kamień';
+  } else if(playerInput == '2')
+      {playerMove = 'papier'
+  }else if(playerInput == '3'){
+      playerMove = 'nozyce'
+  }
+printMessage('Twój ruch to: ' + playerMove);
