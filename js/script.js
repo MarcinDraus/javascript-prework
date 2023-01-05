5456037e369185acad5566e237c588fbc0b7dc40
MarcinDraus <marcin.drauss@gmail.com>
2023-01-05 16:41:02 +0100

    correct 5.4 task

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
-if(randomNumber == 1){
+    if(randomNumber == 1){
   computerMove = 'kamień';
-}
-else if(randomNumber == 2) {
+    }
+    else if(randomNumber == 2) {
     computerMove = 'papier';
-}
-else if(randomNumber == 3) {
+    }
+    else if(randomNumber == 3) {
     computerMove = 'nozyce'
-}
-printMessage('Mój ruch to: ' + computerMove);
+    }
+    printMessage('Mój ruch to: ' + computerMove);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
-if(playerInput == '1'){
+    if(playerInput == '1'){
     playerMove = 'kamień';
-  } else if(playerInput == '2')
+    } else if(playerInput == '2')
       {playerMove = 'papier'
-  }else if(playerInput == '3'){
+    }else if(playerInput == '3'){
       playerMove = 'nozyce'
-  }
-printMessage('Twój ruch to: ' + playerMove);
+    }
+    printMessage('Twój ruch to: ' + playerMove);
 
-if(computerMove == playerMove){
-  printMessage('remis');
-}else if(computerMove == 'kamień' && playerMove == 'papier'){
-printMessage('Ty wygrywasz!');
-}else if(computerMove == 'papier' && playerMove == 'nozyce'){
-  printMessage('Ty wygrywasz!'); 
-}else if(computerMove == 'nozyce' && playerMove == 'kamień'){
-  printMessage('Ty wygrywasz!');
-} else {
-  printMessage('przegrałeś');
- }
+    if(computerMove == playerMove){
+    printMessage('remis');
+    }else if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+    }else if(computerMove == 'papier' && playerMove == 'nozyce'){
+    printMessage('Ty wygrywasz!'); 
+    }else if(computerMove == 'nozyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+    } else {
+    printMessage('przegrałeś');
+    }
